33d5596b7da9cfdf0e9c34e8af79d1a53692475b
vasanth1292 <vasanth1292@gmail.com>
2024-03-26 12:08:46 +0530

    Please scan

---
+++ S3 Demo.tf
@@ -0,0 +1,13 @@
+provider aws {
+  region = us-west-2  # Specify your AWS region
+}
+
+resource aws_s3_bucket my_bucket {
+  bucket = my-unique-bucket-name  # Ensure this name is unique
+  acl    = private
+
+  tags = {
+    Name        = My bucket
+    Environment = Dev
+  }
+}
