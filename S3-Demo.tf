5091f6cf70b8c3a2162616ff3b482f0807b719aa
tarshyak <115622018+tarshyak@users.noreply.github.com>
2024-03-26 14:55:59 +0530

    Update S3-Demo.tf

--- S3-Demo.tf
+++ S3-Demo.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "bucket6971" # Ensure this is unique
-  acl    = "public-read" # Defines the access control level
+  acl    = "private" # Defines the access control level
 
   tags = {
     Name        = "My Terraform S3 Bucket"
