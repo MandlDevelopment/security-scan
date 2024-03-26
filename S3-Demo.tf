d4304d0bd82ceb7914653078455548be0ceae343
vasanth1292 <vasanth1292@gmail.com>
2024-03-26 13:45:16 +0530

    S3 with Public access

--- S3-Demo.tf
+++ S3-Demo.tf
@@ -1,13 +1,26 @@
 provider "aws" {
-  region = "us-west-2"  # Specify your AWS region
+  region = "us-west-2" # Change this to your desired region
 }
-
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name"  # Ensure this name is unique
-  acl    = "private"
-
+ 
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "your-bucket-name" # Change this to your desired bucket name
+  acl    = "public-read"      # Grant public read access to the bucket
+ 
   tags = {
-    Name        = "Prisma Demo"
-    Environment = "Development env"
+    Name = "Example Bucket"
   }
+}
+ 
+resource "aws_s3_bucket_policy" "example_bucket_policy" {
+  bucket = aws_s3_bucket.example_bucket.id
+ 
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = "s3:GetObject",
+      Resource  = aws_s3_bucket.example_bucket.arn
+    }]
+  })
 }
