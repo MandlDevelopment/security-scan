8cb20875897f2514f82d0eebeefc549df24b09d4
Vasanth1292 <106997085+Vasanth1292@users.noreply.github.com>
2024-03-26 15:20:54 +0530

    Update S3-Demo.tf

--- S3-Demo.tf
+++ S3-Demo.tf
@@ -1,20 +1,26 @@
 provider "aws" {
-  region = "us-east-1" # Choose your AWS region
+  region = "us-west-2" # Change this to your desired region
 }
 
-resource "aws_s3_bucket" "data" {
-  # bucket is public
-  # bucket is not encrypted
-  # bucket does not have access logs
-  # bucket does not have versioning
-  bucket        = "Bucket360"
-  tags = {
-    Name        = "Bucket360"
-    yor_trace   = "db7dfc12-0e81-4091-8b21-e8817dceae28"
-    Environment = "Dev"
-    yor_name    = "data"
-  }
-    }, 
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "Prisma-Bucket" # Change this to your desired bucket name
+  acl    = "public-read"      # Grant public read access to the bucket
 
-  )
+  tags = {
+    Name = "Example Bucket"
+  }
+}
+
+resource "aws_s3_bucket_policy" "example_bucket_policy" {
+  bucket = aws_s3_bucket.example_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = "s3:GetObject",
+      Resource  = aws_s3_bucket.example_bucket.arn
+    }]
+  })
 }
