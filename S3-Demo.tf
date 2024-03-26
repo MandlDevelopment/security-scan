fe5e11eaf52821e0209d2319ee4f80b8e8ac5288
tarshyak <115622018+tarshyak@users.noreply.github.com>
2024-03-26 14:54:05 +0530

    Update S3-Demo.tf

--- S3-Demo.tf
+++ S3-Demo.tf
@@ -1,37 +1,13 @@
 provider "aws" {
-  region = "us-west-2" # Change this to your desired region
+  region = "us-east-1" # Choose your AWS region
 }
- 
+
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "bucket-6970" # Adjusted bucket name to lowercase
- 
+  bucket = "bucket6971" # Ensure this is unique
+  acl    = "public-read" # Defines the access control level
+
   tags = {
-    Name = "Example Bucket"
+    Name        = "My Terraform S3 Bucket"
+    Environment = "Dev"
   }
-}
-
-resource "aws_s3_bucket" "example_bucket_log_bucket" {
-  bucket = "example-bucket-log-bucket" # Adjusted bucket name to lowercase and hyphens
-}
-
-resource "aws_s3_bucket_logging" "my_bucket_logging" {
-  bucket = aws_s3_bucket.my_bucket.id
-
-  target_bucket = aws_s3_bucket.example_bucket_log_bucket.id
-  target_prefix = "log/"
-}
-
- 
-resource "aws_s3_bucket_policy" "my_bucket_policy" {
-  bucket = aws_s3_bucket.my_bucket.id
- 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect    = "Allow",
-      Principal = "*",
-      Action    = "s3:GetObject",
-      Resource  = aws_s3_bucket.my_bucket.arn
-    }]
-  })
 }
