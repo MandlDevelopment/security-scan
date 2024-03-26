e38ec0d96feb04c778db67b92f6f923239594e15
vasanth1292 <vasanth1292@gmail.com>
2024-03-26 13:33:09 +0530

    Now compliant

--- S3-Demo.tf
+++ S3-Demo.tf
@@ -1,13 +1,13 @@
-provider aws {
-  region = us-west-2  # Specify your AWS region
+provider "aws" {
+  region = "us-west-2"  # Specify your AWS region
 }
 
-resource aws_s3_bucket my_bucket {
-  bucket = my-unique-bucket-name  # Ensure this name is unique
-  acl    = private
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name"  # Ensure this name is unique
+  acl    = "private"
 
   tags = {
-    Name        = Prisma Demo
-    Environment = Development env
+    Name        = "Prisma Demo"
+    Environment = "Development env"
   }
 }
