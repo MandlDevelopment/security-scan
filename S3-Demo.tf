e3da91a3a2df7bcbfc39d81dc02b618b79a4d96b
Vasanth1292 <106997085+Vasanth1292@users.noreply.github.com>
2024-03-26 15:42:39 +0530

    Update S3-Demo.tf

--- S3-Demo.tf
+++ S3-Demo.tf
@@ -2,17 +2,17 @@ provider "aws" {
   region = "us-west-2" # Change this to your desired region
 }
 
-resource "aws_s3_bucket" "sample_bucket" {
+resource "aws_s3_bucket" "demo_bucket" {
   bucket = "prisma-bucket" # Change this to your desired bucket name
   #acl    = "private"      # Grant public read access to the bucket
 
   tags = {
-    Name = "sample Bucket"
+    Name = "demo Bucket"
   }
 }
 
-resource "aws_s3_bucket_policy" "example_bucket_policy" {
-  bucket = aws_s3_bucket.sample_bucket.id
+resource "aws_s3_bucket_policy" "demo_bucket_policy" {
+  bucket = aws_s3_bucket.demo_bucket.id
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -20,7 +20,7 @@ resource "aws_s3_bucket_policy" "example_bucket_policy" {
       Effect    = "Allow",
       Principal = "*",
       Action    = "s3:GetObject",
-      Resource  = aws_s3_bucket.sample_bucket.arn
+      Resource  = aws_s3_bucket.demo_bucket.arn
     }]
   })
 }
