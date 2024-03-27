a789617497829584296b7c1fe0668003cbbfce6b
vasanth1292 <vasanth1292@gmail.com>
2024-03-27 12:17:50 +0530

    subnet changed

--- service.tf
+++ service.tf
@@ -40,7 +40,7 @@ resource "aws_internet_gateway" "my_igw" {
 # Create a subnet within the VPC
 resource "aws_subnet" "my_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
 
   tags = {
